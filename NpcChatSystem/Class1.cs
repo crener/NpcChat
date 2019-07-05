74e7cdddf63b05fd45ba54ffe7996e7a3ebde782
Martin Kruger <martin661070@gmail.com>
2019-07-05 22:51:04 +0100

    And so it begins

---
+++ NpcChatSystem/Class1.cs
@@ -0,0 +1,4 @@
+﻿namespace NpcChatSystem
+{
+    public class Class1 { }
+}
---
+++ NpcChatUi/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace NpcChat
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App { }
+}
