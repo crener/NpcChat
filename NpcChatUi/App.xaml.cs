392e88918294f4789019f7277e829016a5740409
Martin Kruger <martin661070@gmail.com>
2019-09-26 00:29:50 +0100

    Fixed scrolling issue, added Jit caching

--- NpcChatUi/App.xaml.cs
+++ NpcChatUi/App.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace NpcChat
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime;
+using System.Windows;
+
+namespace NpcChat
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        public App()
+        {
+            List<AssemblyTitleAttribute> title = Assembly.GetAssembly(typeof(App)).GetCustomAttributes<AssemblyTitleAttribute>().ToList();
+            string path = Path.Combine(Path.GetTempPath(), title[0].Title);
 
+            ProfileOptimization.SetProfileRoot(path);
+            ProfileOptimization.StartProfile("jit-profile.cache");
+        }
     }
 }
---
+++ NpcChatUi/Views/Utility/Behaviour/IgnoreMouseWheelBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace NpcChat.Views.Utility.Behaviour
+{
+    //Source https://stackoverflow.com/questions/1585462/bubbling-scroll-events-from-a-listview-to-its-parent
+
+    public sealed class IgnoreMouseWheelBehavior : Behavior<UIElement>
+    {
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseWheel += AssociatedObject_PreviewMouseWheel;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseWheel -= AssociatedObject_PreviewMouseWheel;
+            base.OnDetaching();
+        }
+
+        void AssociatedObject_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+            e.Handled = true;
+
+            var e2 = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+            e2.RoutedEvent = UIElement.MouseWheelEvent;
+            AssociatedObject.RaiseEvent(e2);
+
+        }
+    }
+}
